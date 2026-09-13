900a6af47dcf950e77ecd3ca1ce903dd545ca368
Doobis578 <doobis69@gmail.com>
2026-09-13 13:32:02 +0200

    Fix final list index subscript syntax bug

--- scanner.py
+++ scanner.py
@@ -190,8 +190,8 @@ class AntivirusApp:
                 sel = q_listbox.get(q_listbox.curselection())
                 src = os.path.join(QUARANTINE_DIR, sel)
                 parts = sel.split("_", 1)
-                # Fixed list subscript index target instead of the parent list object
-                orig_name = parts.replace(".locked", "") if len(parts) > 1 else sel.replace(".locked", "")
+                # FIXED: Correct list indexing targets parts[1] cleanly so PyInstaller can build it
+                orig_name = parts[1].replace(".locked", "") if len(parts) > 1 else sel.replace(".locked", "")
                 dst = os.path.join(self.target_folder, orig_name)
                 shutil.move(src, dst)
                 refresh_q_list()
